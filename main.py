03d249fe01505af0131f45e4cbcb6bec31c84d83
viniciusfinger <fingervinicius@icloud.com>
2025-03-25 15:44:11 -0300

    Change to 🇧🇷

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ graph = create_graph()
 
 config = {"recursion_limit": 5}
 
-response = graph.invoke({"user_question": "What is the capital of France?"}, config=config)
+response = graph.invoke({"user_question": "What is the capital of Brasil?"}, config=config)
 
 print(response)
